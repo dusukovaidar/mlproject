5e36e33b6b5f835adb8a0c2b1e6b1a25e9d4b918
dusukovaidar <dusukov.a@mail.ru>
2025-04-01 18:24:23 +0200

    Model Trainer completed

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -24,10 +24,10 @@ class ModelTrainerConfig:
     trained_model_file_path = os.path.join("artifacts", "model.pkl")
 
 class ModelTrainer:
-    def _init__(self):
+    def __init__(self):
         self.model_trainer_config = ModelTrainerConfig()
 
-    def initiate_model_trainer(self, train_array, test_array, preprocessor_path):
+    def initiate_model_trainer(self, train_array, test_array):
         try:
             logging.info("Splitting training and test input data")
             X_train, y_train, X_test, y_test = (train_array[:, :-1], train_array[:, -1], test_array[:, :-1], test_array[:, -1])
@@ -58,14 +58,16 @@ class ModelTrainer:
 
             logging.info(f"Best found model on both trainig and testing dataset")
 
-            save_object(file_path = self.model_trainer_config.trained_model_file_path,
-                        obj = best_model)
+            save_object(
+                file_path = self.model_trainer_config.trained_model_file_path,
+                obj = best_model
+            )
             
             predicted = best_model.predict(X_test)
 
             r2_square = r2_score(y_test, predicted)
 
-            return r2_square
+            return r2_square, best_model_name
 
 
         except Exception as e:
