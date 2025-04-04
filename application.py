3286c2abadb444a79756feebf6b9aef3283c99fd
Dusukov Aidar <95483918+dusukovaidar@users.noreply.github.com>
2025-04-04 23:46:36 +0200

    Update application.py

--- application.py
+++ application.py
@@ -4,9 +4,7 @@ import numpy as np
 import pandas as pd
 
 from sklearn.preprocessing import StandardScaler
-
 from src.pipeline.predict_pipeline import CustomData, PredictPipeline
-
 
 application = Flask(__name__)
 app = application
